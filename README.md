368dcf69c0935c7dc8cb3a699021e849b4abe539
Saurabh Patni <rpssaurabhpatni@gmail.com>
2018-04-03 23:56:14 +0530

    Added support for default gravatar image, Changed React.proptypes to prop-types. (#7)
    * Add: additional props to handle empty email addres, prop-types instead of deprecated React.proptypes
    * Add: table of available props for the gravatar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## React Native Gravatar component
+# React Native Gravatar component
 
 [![npm version](https://img.shields.io/npm/v/react-native-avatar-gravatar.svg?style=flat-square)](https://www.npmjs.com/package/react-native-avatar-gravatar)
 [![npm downloads](https://img.shields.io/npm/dm/react-native-avatar-gravatar.svg?style=flat-square)](https://www.npmjs.com/package/react-native-avatar-gravatar)
@@ -9,10 +9,10 @@ React Native Gravatar is a simple component that loads the avatar with an option
 - [Installation](#installation)
 
 
-### Installation
+## Installation
 `npm install react-native-avatar-gravatar`
 
-### Usage
+## Usage
 ```javascript
 import Gravatar from 'react-native-avatar-gravatar';
 
@@ -24,3 +24,15 @@ class Application extends Component {
   }
 })
 ```
+
+## Properties
+
+### Basic
+
+| Prop  | Default  | Type | Description |
+| :------------ |:---------------:| :---------------:| :-----|
+| emailAddress | '' | `string` | Email address for gravatar, if not provided uses default image for gravatar |
+| size | 60 | `number` | Size for the gravatar image |
+| mask | 'circle' | `number` | Mask for the gravatar image. One of - 'circle', 'square', 'rounded' |
+| defaultImage | 'retro' | `string` | default image if no email address. One of - '404', 'mm', 'identicon', 'monsterid', 'wavatar', 'retro', 'robohash', 'blank' |
+| resizeMode | 'contain' | `string` | resizeMode for the gravatar image |
--- index.js
+++ index.js
@@ -4,11 +4,15 @@ import {
     View,
     Image
 } from 'react-native';
+import PropTypes from "prop-types";
 
 import md5 from 'blueimp-md5';
 
 const GRAVATAR_URI = 'www.gravatar.com/avatar/';
 
+/**
+ *@description Display gravatar for an email address
+ */
 class Gravatar extends React.Component {
 
     _calculateStyle() {
@@ -32,27 +36,39 @@ class Gravatar extends React.Component {
     render() {
         const protocol = this.props.secure ? 'https://' : 'http://';
 
-        const uri = protocol + GRAVATAR_URI + md5(this.props.emailAddress) + '?s=' + this.props.size;
+        const uri = protocol + GRAVATAR_URI + md5(this.props.emailAddress) + '?s=' + this.props.size + '&d=' + this.props.defaultImage;
         const style = this._calculateStyle();
         return (
             <View style={[styles.overlay]}>
-                <Image source={{uri}} style={[styles.image, style]} />
+                <Image resizeMode={this.props.resizeMode} source={{uri}} style={[styles.image, style]} />
             </View>
         );
     }
 }
 
 Gravatar.propTypes = {
+    /**
+     * @param {string} emailAddress - email address for the gravatar
+     * @param {number} size - size of the gravatar
+     * @param {('circle'|'square'|'rounded' )} mask - mask type for the gravatar
+     * @param {('404'|'mm'|'identicon'|'monsterid'|'wavatar'|'retro'|'robohash'|'blank')} defaultImage - deafult image type if no image is supplied
+     * @param {('cover'|'contain'|'stretch'|'repeat'|'center')} resizeMode - resizeMode for the gravatar image.
+     */
     ...View.PropTypes,
-    emailAddress: React.PropTypes.string.isRequired,
-    size: React.PropTypes.number.isRequired,
-    mask: React.PropTypes.oneOf(['circle', 'square', 'rounded'])
+    emailAddress: PropTypes.string,
+    size: PropTypes.number,
+    mask: PropTypes.oneOf(['circle', 'square', 'rounded']),
+    defaultImage: PropTypes.oneOf(['404', 'mm', 'identicon', 'monsterid', 'wavatar', 'retro', 'robohash', 'blank']),
+    resizeMode: PropTypes.oneOf(['cover', 'contain', 'stretch', 'repeat', 'center'])
 };
 
 Gravatar.defaultProps = {
+    emailAddress: '',
     size: 600,
     mask: 'circle',
-    secure: true
+    secure: true,
+    defaultImage: 'retro',
+    resizeMode: 'contain'
 };
 
 const styles = StyleSheet.create({
